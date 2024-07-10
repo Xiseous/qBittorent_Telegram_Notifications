fad513c97a80b1f7c3a904c83ea3ec252a2a7c23
Xiseous <64169979+Xiseous@users.noreply.github.com>
2024-07-10 03:58:44 +0200

    Update qbittorrent_telegram_notification.sh
    Escape of special characters in names such as &,
    
    Updated error message with bold text for coverage (Telegram does not support colors, so no color tags)

--- src/qbittorrent_telegram_notification.sh
+++ src/qbittorrent_telegram_notification.sh
@@ -11,18 +11,18 @@ SIZE_TEXT="Size: "
 CATEGORY_TEXT="Category: "
 TRACKERS_TEXT="Tracker: "
 
-# Receive specific arguments from Script Execution (!!!add stuff here that you want)
+# Receive specific arguments from Script Execution
 TORRENT_NAME="$1"
 CATEGORY="$2"
 TORRENT_SIZE_BYTES="$3"
 CURRENT_TRACKER="$4"
 
-# Remove quotation marks from TORRENT_SIZE_BYTES (without this the following conversion fails)
+# Remove quotation marks from TORRENT_SIZE_BYTES
 TORRENT_SIZE_BYTES=$(echo "$TORRENT_SIZE_BYTES" | sed 's/[“”"]//g')
 
-# Check if TORRENT_SIZE_BYTES is set and is a number (make sure we removed the special characters)
+# Check if TORRENT_SIZE_BYTES is set and is a number
 if ! [[ "$TORRENT_SIZE_BYTES" =~ ^[0-9]+$ ]]; then
-    TORRENT_SIZE="Error: Invalid size"
+    TORRENT_SIZE="⛔️<b>ERROR</b> - Calculation Failed⛔️"
     SIZE_UNIT=""
 else
     # Convert size from bytes to MB or GB as appropriate
@@ -37,9 +37,18 @@ else
     fi
 fi
 
+# Escape special characters in the input variables
+escape() {
+    echo "$1" | sed 's/&/%26/g; s/ /%20/g; s/\n/%0A/g'
+}
+
+ESCAPED_TORRENT_NAME=$(escape "$TORRENT_NAME")
+ESCAPED_CATEGORY=$(escape "$CATEGORY")
+ESCAPED_CURRENT_TRACKER=$(escape "$CURRENT_TRACKER")
+
 # Notification message
 # If you need a line break, use "%0A" instead of "\n".
-MESSAGE="%0A${MESSAGE_TEXT} %0A${NAME_TEXT}${TORRENT_NAME} %0A${CATEGORY_TEXT}${CATEGORY} %0A${SIZE_TEXT}${TORRENT_SIZE} ${SIZE_UNIT} %0A${TRACKERS_TEXT}${CURRENT_TRACKER}"
+MESSAGE="%0A${MESSAGE_TEXT} %0A${NAME_TEXT}${ESCAPED_TORRENT_NAME} %0A${CATEGORY_TEXT}${ESCAPED_CATEGORY} %0A${SIZE_TEXT}${TORRENT_SIZE} ${SIZE_UNIT} %0A${TRACKERS_TEXT}${ESCAPED_CURRENT_TRACKER}"
 
 # Prepares the request url
 TG_WEBHOOK_URL="https://api.telegram.org/bot${BOT_TOKEN}/sendMessage"
@@ -47,14 +56,13 @@ TG_WEBHOOK_URL="https://api.telegram.org/bot${BOT_TOKEN}/sendMessage"
 # Prepares the directory for saving the logs
 BASEDIR=$(dirname "$0")
 
-# Send Notification to Bot, Generate Debug Log with Response at basedir.
+# Sends the notification to the telegram bot and saves the response content into the notificationsLog.txt
 curl -S -X POST \
   -d chat_id="${CHAT_ID}" \
   -d text="${MESSAGE}" \
   -d parse_mode="HTML" \
-# Remove " | tee -a "${BASEDIR}/notificationsLog.txt" to stop generating a debug log
   "${TG_WEBHOOK_URL}" -w "\n" | tee -a "${BASEDIR}/notificationsLog.txt"
 
-# Prints an info message in the console / useless idk why he added this
+# Prints an info message in the console
 TR_TIME_LOCALTIME=$(date)
 echo "[${TR_TIME_LOCALTIME}]-[${TORRENT_NAME}] Download completed. Telegram notification sent."
